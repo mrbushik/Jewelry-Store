3165611da900782f36b4424abf6e500d5d44f434
Bushuev Nikita <mrbushik1@gmail.com>
2022-12-13 12:50:19 +0300

    delete products in to the cart

--- src/Components/Pages/cart.tsx
+++ src/Components/Pages/cart.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { productItem } from "../interfaces";
 import CartItems from "../layout/cartItems";
+import { deleteProductInCart } from "../redux/actions/cart";
 
 const Cart: React.FC = () => {
+  const dispatch: any = useDispatch();
+
   const cartItems = useSelector((state: any) => state.cart.cartItems);
+  const handleDelete = (item: productItem) => {
+    dispatch(
+      deleteProductInCart(
+        cartItems.filter(
+          (product: productItem) => product.imageLink !== item.imageLink
+        )
+      )
+    );
+  };
   return (
     <div>
       <h3 className="text-center mt-4">Корзина с товаром</h3>
       <div className="d-flex justify-content-center mt-5 cart-list">
-        {cartItems.map((item: productItem, index: number) => (
-          <CartItems item={item} key={index} />
-        ))}
+        {!!cartItems.length ? (
+          cartItems.map((item: productItem, index: number) => (
+            <CartItems item={item} key={index} onDelete={handleDelete} />
+          ))
+        ) : (
+          <p className="text-danger mt-5">Вы не выбрали не одного товара</p>
+        )}
       </div>
     </div>
   );
--- src/Components/layout/cartItems.tsx
+++ src/Components/layout/cartItems.tsx
@@ -3,16 +3,20 @@ import { productItem } from "../interfaces";
 
 type cartItemsProps = {
   item: productItem;
+  onDelete(item: productItem): void;
 };
 
-const CartItems: React.FC<cartItemsProps> = ({ item }) => {
+const CartItems: React.FC<cartItemsProps> = ({ item, onDelete }) => {
   return (
     <div className=" my-3 d-flex align-items-center">
       <img className="cart-img me-3" src={item.imageLink} />
-      <p className="pt-2" >{item.title}</p>
+      <p className="pt-2">{item.title}</p>
       <div className="d-flex justify-content-between">
-        <p className='pt-2 ms-2'>{item.price} BYN</p>
-        <div className="text-danger ms-3 pointer  mt-2">
+        <p className="pt-2 ms-2">{item.price} BYN</p>
+        <div
+          className="text-danger ms-3 pointer  mt-2"
+          onClick={() => onDelete(item)}
+        >
           Удалить
         </div>
       </div>
