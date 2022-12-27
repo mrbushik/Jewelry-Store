4a5d4cf4a72c95c425c5e92cdb7ec3b4757c789f
Bushuev Nikita <mrbushik1@gmail.com>
2022-12-27 17:56:45 +0300

    edit product page

--- src/Components/pages/productPage.tsx
+++ src/Components/pages/productPage.tsx
@@ -6,12 +6,18 @@ import {
   currentProduct,
   currentProductsRequest,
 } from "../redux/actions/productData";
+import { log } from "util";
 
 const ProductPage: React.FC = () => {
   const params: any = useParams();
   const location = useLocation();
   const history = useHistory();
   const dispatch: any = useDispatch();
+
+  enum metals {
+    gold = "Золото",
+    silver = "Серебро",
+  }
 
   const MANS_JEWELRY_URL =
     "https://jewelry-store-3488f-default-rtdb.europe-west1.firebasedatabase.app/Products/mens.json";
@@ -33,8 +39,6 @@ const ProductPage: React.FC = () => {
     location.pathname.includes("mans") ? getMansProducts() : getWomanProducts();
   }, []);
 
-  // useEffect()
-
   const getMansProducts = () => {
     if (mansJewelryItems.length)
       return dispatch(currentProduct(mansJewelryItems[params.Id]));
@@ -52,11 +56,28 @@ const ProductPage: React.FC = () => {
       <div className="btn btn-outline-primary m-4" onClick={history.goBack}>
         Назад
       </div>
-      <h1 className="mt-3 text-center">{currentProductData.title}</h1>
-      <img className="mt-3" src={currentProductData.imageLink} />
-      <p className="fs-5 mt-3 ms-5">
-        Цена: <span className="fw-bolder">{currentProductData.price} BYN</span>
-      </p>
+      <div className="d-flex flex-wrap justify-content-center ">
+        <div>
+          <img
+            className="mt-3 product-page__img"
+            src={currentProductData.imageLink}
+          />
+        </div>
+        <div className="ms-3">
+          <h3 className="mt-3 text-center fw-semibold">
+            {currentProductData.title}
+          </h3>
+
+          <p className="mt-5">
+            Метал: {currentProductData.metal === "gold" ? "Золото" : "Серебро"}
+          </p>
+
+          <p className="fs-5 mt-3 ">
+            Цена:
+            <span className="fw-bolder">{currentProductData.price} BYN</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
